d2d88fbcd50df89fa115d0586badab0d45a5fcbc
Kieran Donnelly <kierandonnelly2003@gmail.com>
2025-02-08 21:33:07 +0000

    Allow no url in course creation

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,5 +2,5 @@ class Course < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   validates :ordering, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, uniqueness: true
-  validates :image_url, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp, message: "must be a valid URL" }
+  validates :image_url, format: { with: URI::DEFAULT_PARSER.make_regexp, message: "must be a valid URL" }, allow_blank: true
 end
