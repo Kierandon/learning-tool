2864e6fb37bca5c462eef5dc88abfb3f7582b25f
Kieran Donnelly <kierandonnelly2003@gmail.com>
2025-02-08 16:57:26 +0000

    Add image_url to course fixtures and create homepage system test

---
+++ test/system/homepage_test.rb
@@ -0,0 +1,19 @@
+require "application_system_test_case"
+
+class HomepageTest < ApplicationSystemTestCase
+  test "visiting the homepage" do
+    visit root_url
+
+    Course.all.each do |course|
+      assert_selector "h2", text: course.title
+      assert_selector "p", text: course.description
+      if course.image_url.nil?
+        assert_selector "img[src='https://placehold.co/600x400']"
+      else
+        assert_selector "img[src='#{course.image_url}']"
+      end
+    end
+
+    assert_selector "h1", text: "Available Courses"
+  end
+end
