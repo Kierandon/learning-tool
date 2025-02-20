aafc46dd91943e132cf14d418082269d7896b006
Kieran Donnelly <kierandonnelly2003@gmail.com>
2025-02-20 13:30:18 +0000

    Add before_create callback to set position for new steps

--- app/models/step.rb
+++ app/models/step.rb
@@ -3,6 +3,8 @@ class Step < ApplicationRecord
   has_many :questions, -> { order(position: :asc) }, dependent: :destroy
   has_rich_text :content
 
+  before_create :set_position
+
   def previous_step
     course.steps.find_by(position: position - 1)
   end
@@ -13,5 +15,11 @@ class Step < ApplicationRecord
 
   def all_questions_answered?(user)
     questions.all? { |question| question.answered_correctly?(user) }
+  end
+
+  private
+
+  def set_position
+    self.position = course.steps.maximum(:position).to_i + 1
   end
 end
