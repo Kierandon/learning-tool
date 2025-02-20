966691172bba0603c5bea16699195322e382a619
Kieran Donnelly <kierandonnelly2003@gmail.com>
2025-02-20 14:11:41 +0000

    remove unused code

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -30,28 +30,6 @@ class AnswersController < ApplicationController
     answer_service.process(permitted_answer)
   end
 
-  def handle_correct_answer
-    if next_step
-      advance_to_next_step
-    else
-      complete_course
-    end
-  end
-
-  def handle_incorrect_answer
-    redirect_to course_step_path(@course, @step), alert: "Incorrect!"
-  end
-
-  def advance_to_next_step
-    @progression.update!(current_step: next_step)
-    redirect_to course_step_path(@course, next_step), notice: "Correct!"
-  end
-
-  def complete_course
-    redirect_to course_path(@course),
-      notice: "Course completed! #{@question.questionable.success_message}"
-  end
-
   def next_step
     @next_step ||= @step.next_step || @course.steps.last
   end
