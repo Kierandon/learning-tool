a8607e75e81ff29a537d783892ddc92080c7b296
Kieran Donnelly <kierandonnelly2003@gmail.com>
2025-03-05 18:44:54 +0000

    update initializers

--- config/initializers/action_text.rb
+++ config/initializers/action_text.rb
@@ -1,3 +1,13 @@
 default_allowed_tags = Class.new.include(ActionText::ContentHelper).new.sanitizer_allowed_tags
-ActionText::ContentHelper.allowed_tags = default_allowed_tags.add("iframe")
-ActionText::ContentHelper.allowed_tags = default_allowed_tags.add("style")
+default_allowed_attributes = Class.new.include(ActionText::ContentHelper).new.sanitizer_allowed_attributes
+
+new_allowed_tags = default_allowed_tags + %w[
+  iframe style table thead tbody tr th td caption colgroup col
+]
+
+new_allowed_attributes = default_allowed_attributes + %w[
+  style href target id class aria-label aria-describedby aria-hidden scope colspan rowspan
+]
+
+ActionText::ContentHelper.allowed_tags = new_allowed_tags
+ActionText::ContentHelper.allowed_attributes = new_allowed_attributes
