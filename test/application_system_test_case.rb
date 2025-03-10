f708d1a5620b67e2650ab7230eeb764bffea2367
Kieran Donnelly <kierandonnelly2003@gmail.com>
2025-03-10 23:23:50 +0000

    wcag aaa and course fixes

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -4,7 +4,9 @@ require "axe/matchers/be_axe_clean"
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
 
-  def assert_accessible(page, matcher = Axe::Matchers::BeAxeClean.new.according_to(:wcag21aa, "best-practice"))
+  def assert_accessible(page, matcher = Axe::Matchers::BeAxeClean.new.according_to(
+    Axe::Matchers::BeAxeClean.new.according_to(:wcag2a, :wcag2aa, :wcag21a, :wcag21aa, :wcag2aaa)
+    ))
     audit_result = matcher.audit(page)
     assert(audit_result.passed?, audit_result.failure_message)
   end
--- test/system/accessibility_test.rb
+++ test/system/accessibility_test.rb
@@ -2,8 +2,8 @@ require "application_system_test_case"
 
 class AccessibilityTest < ApplicationSystemTestCase
   setup do
-    StandardLoader.load_from_yaml(Rails.root.join("db/seeds/courses/iso29119_standard.yml"))
-    CourseLoader.load_from_yaml(Rails.root.join("db/seeds/courses/iso29119_course.yml"))
+    @standard = StandardLoader.load_from_yaml(Rails.root.join("db/seeds/courses/iso29119_standard.yml"))
+    @course = CourseLoader.load_from_yaml(Rails.root.join("db/seeds/courses/iso29119_course.yml"))
   end
 
   test "homepage should be accessible" do
@@ -13,14 +13,26 @@ class AccessibilityTest < ApplicationSystemTestCase
   end
 
   test "standard page should be accessible" do
-    visit courses_standard_path(Standard.first)
+    visit courses_standard_path(@standard)
 
     assert_accessible(page)
   end
 
-  test "course page should be accessible" do
-    visit course_path(Course.first)
+  test "iso29119 course should be accessible" do
+    user = User.create!
+    visit new_session_path
+    fill_in "login_hash", with: user.login_hash
+    click_button "Login"
+    assert_current_path root_path
+    visit confirm_start_course_path(@course)
 
+    @course.steps.each do |step|
+      p "Visiting step: #{step.title}"
+      visit course_step_path(@course, step)
+      assert_accessible(page)
+    end
+
+    visit complete_course_path(@course)
     assert_accessible(page)
   end
 end
