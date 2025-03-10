53229f9829797270ba80c61db24844aa6c3acd37
Kieran Donnelly <kierandonnelly2003@gmail.com>
2025-03-10 13:56:20 +0000

    cleanup code

--- app/components/question_step_component.rb
+++ app/components/question_step_component.rb
@@ -4,4 +4,20 @@ class QuestionStepComponent < ViewComponent::Base
     @course = course
     @just_answered = just_answered
   end
+
+  def render_core_question_icon?
+    @step.learning_objectives.any?
+  end
+
+  def questions
+    @step.questions
+  end
+
+  def all_questions_answered?(user)
+    @step.all_questions_answered?(user)
+  end
+
+  def form_url
+    course_step_answers_path(@course, @step)
+  end
 end
